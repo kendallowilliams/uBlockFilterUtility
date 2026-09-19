9583564c6b7d263c66e2cd657184c2c9ebb9c8ab
kendallowilliams <kendallowilliams@outlook.com>
2026-09-18 20:12:46 -0400

    base href fix

--- ublockfilterutility.client/src/app/app-module.ts
+++ ublockfilterutility.client/src/app/app-module.ts
@@ -38,8 +38,7 @@ import { APP_BASE_HREF } from '@angular/common';
       provide: APP_BASE_HREF,
       useFactory: () => {
         const segments = window.location.pathname.split('/').filter(Boolean);
-        const path = segments.length > 0 ? `${segments[0]}/` : '/';
-        return `/${path}`;
+        return segments.length > 0 ? `${segments[0]}/` : '/';
       }
     }
   ],
